6e68cadfb20fac9fd43ef9995367155c06d504c9
delkato <delgado@gatech.edu>
2017-09-21 01:24:09 -0400

    Trying things that aren't working

--- public/js/teacher.js
+++ public/js/teacher.js
@@ -1,10 +1,24 @@
-$(document).ready(function () {
+document.addEventListener('DOMContentLoaded', function() {
   $("tbody.connect-groups").sortable({
-      connectWith: ".connect-groups",
-      items: "> tr:not(:first)",
-      appendTo: "parent",
-      helper: "clone",
-      cursor: "move",
-      zIndex: 999990
+    connectWith: ".connect-groups",
+    items: "> tr:not(:first)",
+    appendTo: "parent",
+    helper: "clone",
+    cursor: "move",
+    zIndex: 999990
+  });
+
+  $('#addClass').submit(function(event){
+    $(this).find("#className").prop('disabled', true);
+    console.log("submit to Firebase");
+    var titleToSend = $('#classNameTextBox').val();
+    var newClass = {
+      titleToSend: []
+    }
+    var classesRef = ref.child('Classes');
+    var classesRef = classesRef.push();
+    console.log('Class key', classesRef.key);
+    var classRef = classesRef.push(newClass);
+    console.log(classesRef);
   });
 });
