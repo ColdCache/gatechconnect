80a280c91c44796e7bfef7972887d7f4a9acee80
delkato <delgado@gatech.edu>
2017-09-30 22:38:51 -0400

    Simplify changing current roster to refer to function instead of string representation

--- public/js/script.js
+++ public/js/script.js
@@ -328,21 +328,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
       var dropDown = document.getElementById("classes");
       currentClass = dropDown.value;
-      dropDown.setAttribute("onchange", "var currentClass = document.getElementById('classes').value; var config={apiKey: 'AIzaSyAhKnwZ_l8jwtMQFc7mBh30l96NLyZq03Q',authDomain: 'gatechconnect.firebaseapp.com',databaseURL: 'https://gatechconnect.firebaseio.com',projectId: 'gatechconnect',storageBucket: 'gatechconnect.appspot.com',messagingSenderId: '671330762711'};" +
-                                            "firebase.initializeApp(config, 'privateFirebase'); var newDB = firebase.database(); var newAuth = firebase.auth(); var currentUid = newAuth.currentUser.uid; var rowNumber = 1;" +
-                                            "var classLoc = 'Classes/' + currentUid + '/' + currentClass; var classRef = newDB.ref(classLoc);" +
-                                            "document.getElementById('ungrouped-students').innerHTML = '<th>First Name</th><th>Last Name</th>';" +
-                                            "if (currentClass.localeCompare('Initial') != 0) {" +
-                                            "classRef.orderByChild('LastName').on('child_added', function(snapshot) {" +
-                                            "var table = document.getElementById('ungroupedStudents');" +
-                                            "var row = table.insertRow(rowNumber);" +
-                                            "var cell1 = row.insertCell(0);" +                                   
-                                            "var cell2 = row.insertCell(1);" +
-                                            "cell1.innerHTML = snapshot.val().FirstName;" +
-                                            "cell2.innerHTML = snapshot.val().LastName;" +
-                                            "rowNumber++;" +
-                                            "});" +
-                                            "} firebase.app('privateFirebase').delete();");
+      dropDown.addEventListener("change", changeRoster);
       classes.orderByKey().on("child_added", function(snapshot) {
         var z = document.createElement("option");
         z.setAttribute("value", snapshot.key);
@@ -890,15 +876,17 @@ function changeRoster() {
   console.log(classLoc);
   var classRef = newDB.ref(classLoc);
   document.getElementById('ungrouped-students').innerHTML = '<th>First Name</th><th>Last Name</th>';
-  classRef.orderByChild('LastName').on('child_added', function(snapshot) {
-    var table = document.getElementById('ungroupedStudents');
-    var row = table.insertRow(rowNumber);
-    var cell1 = row.insertCell(0);                                                 
-    var cell2 = row.insertCell(1);
-    cell1.innerHTML = snapshot.val().FirstName;
-    cell2.innerHTML = snapshot.val().LastName;
-    rowNumber++;
-  }); 
+  if (currentClass.localeCompare('Initial') != 0) {
+    classRef.orderByChild('LastName').on('child_added', function(snapshot) {
+      var table = document.getElementById('ungroupedStudents');
+      var row = table.insertRow(rowNumber);
+      var cell1 = row.insertCell(0);                                                 
+      var cell2 = row.insertCell(1);
+      cell1.innerHTML = snapshot.val().FirstName;
+      cell2.innerHTML = snapshot.val().LastName;
+      rowNumber++;
+    });
+  }
   firebase.app('privateFirebase').delete();
 }
 
