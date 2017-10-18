a2d2e0d7fe49c3288617a688c9f4372dd7d4736c
delkato <delgado@gatech.edu>
2017-10-18 01:46:30 -0400

    Add needed js sources, index js file, add modernizr

---
+++ public/assets/js/index.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // FIREBASE CONFIG
+  var config = {
+    apiKey: "AIzaSyAhKnwZ_l8jwtMQFc7mBh30l96NLyZq03Q",
+    authDomain: "gatechconnect.firebaseapp.com",
+    databaseURL: "https://gatechconnect.firebaseio.com",
+    projectId: "gatechconnect",
+    storageBucket: "gatechconnect.appspot.com",
+    messagingSenderId: "671330762711"
+  };
+  
+  //INITIALIZE FIREBASE WEB APP
+  firebase.initializeApp(config); 
+  var auth = firebase.auth();
+
+  auth.onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      console.log("u logged in nigga");
+    } else {
+      // No user is signed in.
+      $("#dropdown").hide();
+    }
+  });
+});
