d471e2ac21037941ce88950a61ff2e2cb597903c
Quang Vo <kanglaive@users.noreply.github.com>
2017-10-27 17:32:20 -0400

    Registration Functionality

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -14,3 +14,29 @@ $(function() {
 		e.preventDefault();
 	});
 });
+
+document.getElementById('register-submit').addEventListener('click', function() {
+	var auth = firebase.auth();
+	// pull user inputed strings from doc elements
+	var username_input = document.getElementById('username');
+	var username = username_input.value;
+	var email_input = document.getElementById('email');
+	var email = email_input.value;
+	var password_input = document.getElementById('password');
+	var password = password_input.value;
+	var confirm_password_input = document.getElementById('confirm-password');
+	var confirm_password = confirm_password_input.value;
+
+	// create new user in firebase auth
+	var registerPromise = auth.createUserWithEmailAndPassword(email, confirm_password);
+	registerPromise.then(function(user) {
+		user.sendEmailVerification();
+		alert("An email verification was sent to " + email + ".", 5000);
+	}).catch(function(registerError) {
+		console.log(registerError.message);
+	});
+})
+
+document.getElementById('login-submit').addEventListener('click', function() {
+
+})
--- public/assets/js/static.js
+++ public/assets/js/static.js
@@ -1,35 +1,44 @@
-document.addEventListener('DOMContentLoaded', function() {
-  // FIREBASE CONFIG
-  var config = {
-    apiKey: "AIzaSyAhKnwZ_l8jwtMQFc7mBh30l96NLyZq03Q",
-    authDomain: "gatechconnect.firebaseapp.com",
-    databaseURL: "https://gatechconnect.firebaseio.com",
-    projectId: "gatechconnect",
-    storageBucket: "gatechconnect.appspot.com",
-    messagingSenderId: "671330762711"
-  };
-  
-  //INITIALIZE FIREBASE WEB APP
-  if (!firebase.apps.length) {
-    firebase.initializeApp(config);
-  }
-  var auth = firebase.auth();
+// FIREBASE CONFIG
+var config = {
+  apiKey: "AIzaSyAhKnwZ_l8jwtMQFc7mBh30l96NLyZq03Q",
+  authDomain: "gatechconnect.firebaseapp.com",
+  databaseURL: "https://gatechconnect.firebaseio.com",
+  projectId: "gatechconnect",
+  storageBucket: "gatechconnect.appspot.com",
+  messagingSenderId: "671330762711"
+};
 
+//INITIALIZE FIREBASE WEB APP
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+var auth = firebase.auth();
+var database = firebase.database();
+
+
+document.addEventListener('DOMContentLoaded', function() {
   $('#footer').load('footer.html');
   // Wait until navigation bar loads before changing div visibility
   $('#navbar').load('navbar.html', function() {
     auth.onAuthStateChanged(function(user) {
       if (user) {
         // User is signed in.
+        user = auth.currentUser;
         console.log("Logged in!");
         $("#sign-in").hide();
+        $("#teacher-dropdown").show();
+        $("#student-dropdown").show();
+        $("#sign-out").show();
       } else {
         // No user is signed in.
         console.log("NOT logged in!");
+        $("#sign-in").show();
         $("#teacher-dropdown").hide();
         $("#student-dropdown").hide();
         $("#sign-out").hide();
       }
     });
   });
+
+  
 });
