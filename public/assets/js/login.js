9909ead43993075d9be9e5916e0d0ba9aa1eae61
delkato <kdelgado8@gatech.edu>
2017-10-28 02:32:43 -0400

    Login works
    Still need to work on registration

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -13,4 +13,18 @@ $(function() {
 		$(this).addClass('active');
 		e.preventDefault();
 	});
+	$('#login-submit').click(function(e) {
+		var email = $('#email').val();
+		var password = $('#password').val();
+		if (email == "" || password == "") {
+			alert('Please fill in all fields.');
+		} else {
+			firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+				var errorCode = error.code;
+				var errorMessage = error.message;
+				alert(errorCode + ": " + errorMessage);
+			});
+		}
+		e.preventDefault();
+	});
 });
