91cd777a51c54cf54b88488af620ce98716f7a2e
Quang Vo <quangdvo@gmail.com>
2017-11-20 04:33:14 -0500

    Student Dashboard Functionality

--- public/assets/js/surveys.js
+++ public/assets/js/surveys.js
@@ -159,15 +159,16 @@ document.addEventListener('DOMContentLoaded', function () {
     auth.onAuthStateChanged(function (user) {
         if (user) {
             var uid = user.uid;
-            loadSurveys(uid);
             var accountRef = firebase.database().ref().child('users').child(uid);
             accountRef.on('value', function (snap) {
                 accountType = (snap.val().accountType || 'none');
                 if (accountType == 'instructor') {
                     $("#showSurveyForm").show();
                     populateSurveyForm(uid);
+                    loadInstructorSurveys(uid);
                 } else if (accountType == 'student') {
                     $("#showSurveyForm").hide();
+                    loadStudentSurveys(uid);
                 }
             });
         }
@@ -203,7 +204,45 @@ function populateSurveyForm(uid) {
     });
 }
 
-function loadSurveys(uid) {
+function loadStudentSurveys(uid) {
+    var surveysTable = document.getElementById('surveys');
+    var usersClasses = firebase.database().ref('users/' + uid + '/classes');
+
+    usersClasses.orderByKey().on('child_added', function(snapshot) {
+        var classID = snapshot.key;
+
+        var classSurveyRef = firebase.database().ref('classes/' + classID + '/surveys');
+        classSurveyRef.orderByKey().on('child_added', function(snap) {
+            var surveyID = snap.key;
+            var surveyRef = firebase.database().ref('surveys/' + surveyID);
+            
+            surveyRef.on('value', function(survsnap) {
+                var row = surveysTable.insertRow(-1);
+                var surveyName = row.insertCell(0);
+                var name = document.createTextNode(survsnap.val().name);
+                surveyName.appendChild(name);
+                var surveyType = row.insertCell(1);
+                var type = document.createTextNode(survsnap.val().type);
+                surveyType.appendChild(type);
+                var teacherID = survsnap.val().instructor;
+                var instructorRef = firebase.database().ref('users/' + teacherID);
+                
+                instructorRef.on('value', function(insSnap) {
+                    var instructorName = row.insertCell(2);
+                    var instructor = document.createTextNode(insSnap.val().firstName + ' ' + insSnap.val().lastName);
+                    instructorName.appendChild(instructor);
+                    var expirationDate = row.insertCell(3);
+                    var date = document.createTextNode(survsnap.val().date);
+                    expirationDate.appendChild(date);
+                });
+            });
+        });
+        
+    });
+}
+
+
+function loadInstructorSurveys(uid) {
     var surveysTable = document.getElementById('surveys');
     var surveysRef = firebase.database().ref('users/' + uid + '/surveys');
 
