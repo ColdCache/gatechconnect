4b7dbf4044e3a5d506613e430b4083ef1d8885bb
delkato <kdelgado8@gatech.edu>
2017-11-30 17:15:16 -0500

    ahhh

--- public/assets/js/groups.js
+++ public/assets/js/groups.js
@@ -9,23 +9,30 @@ document.addEventListener('DOMContentLoaded', function() {
         groupsRef.orderByKey().on('child_added', function(snapshot) {
             var row = groupsTable.insertRow(groupsNum);
             var groupID = snapshot.key;
-        
+            console.log(groupID);
+            /*
             // iterate through each of the user's groups
             var groupRef = firebase.database().ref().child('groups').child(groupID);
-            groupRef.orderByKey.on('child_added', function(snap) {
+            groupRef.orderByKey().on('child_added', function(snap) {
                 var groupName = row.insertCell(0);
                 var course = row.insertCell(1);
                 var teacher = row.insertCell(2);
                 var members = row.insertCell(3);
             
                 // iterate through the group's data entries
+                var studentRef = firebase.database().ref().child('users').child(uid)
                 studentRef.on('value', function(snap) {
+                    //console.log(snap.val());
                     groupName = (snap.val().name || 'none');
                     course = (snap.val().course || 'none');
                     teacher = (snap.val().teacher || 'none');
+                    /*console.log('group name: '+ groupName);
+                    console.log('course: '+ course);
+                    console.log('teacher: '+ teacher);
                 });
                 groupsNum++;
             });
+            */
         });
     });
 });
